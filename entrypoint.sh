2503108d58c0e4bd8b2570569fb9954865f2195b
Dimitrios Desyllas (pc_magas) <you@example.com>
2016-11-10 15:43:22 +0200

    Wait until mysql gets connected

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,12 @@ chmod 777 /var/moodledata
 
 echo "Installing moodle"
 
+while ! mysqladmin ping -h"$DB_PORT_3306_TCP_ADDR" --silent; do
+    sleep 1
+done
+
 #Loop until sucessfylly installs
-sleep 10 && php /var/www/html/admin/cli/install_database.php \
+php /var/www/html/admin/cli/install_database.php \
           --adminemail=${MOODLE_ADMIN_EMAIL} \
           --adminuser=${MOODLE_ADMIN} \
            --adminpass=${MOODLE_ADMIN_PASSWORD} \
