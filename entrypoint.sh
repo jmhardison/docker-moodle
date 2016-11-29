c1c7f25019042a1ff26e0a31a64ec194d6b0e821
Dimitrios Desyllas (pc_magas) <you@example.com>
2016-11-29 22:08:46 +0200

    Ignoring unwanted files and entrypoint seems to work

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,20 +35,44 @@ if [ "$MOODLE_DB_TYPE" = "mysqli" ] || [ "$MOODLE_DB_TYPE" = "mariadb" ]; then
 : ${MOODLE_DB_PASSWORD:=$DB_ENV_MYSQL_PASSWORD}
   fi
 
-  if [ -z "$MOODLE_DB_PASSWORD" ]; then
-  	echo >&2 'error: missing required MOODLE_DB_PASSWORD environment variable'
-  	echo >&2 '  Did you forget to -e MOODLE_DB_PASSWORD=... ?'
-  	echo >&2
-  	exit 1
-  fi
+elif [ "$MOODLE_DB_TYPE" = "pgsql" ]; then
+
+: ${MOODLE_DB_HOST:=$DB_PORT_5432_TCP_ADDR}
+: ${MOODLE_DB_PORT:=${DB_PORT_5432_TCP_PORT}}
+
+  echo "Waiting for postgresql to start at ${MOODLE_DB_HOST} using port ${MOODLE_DB_PORT}..."
+
+  while ! pg_isready -h ${MOODLE_DB_HOST} -p ${MOODLE_DB_PORT} > /dev/null 2> /dev/null; do
+    echo "Connecting to ${MOODLE_DB_HOST} Failed"
+    sleep 1
+  done
+
+  echo "Setting up the database connection info"
+
+: ${MOODLE_DB_NAME:=${DB_ENV_POSTGRES_DB:-'moodle'}}
+: ${MOODLE_DB_USER:=${DB_ENV_POSTGRES_USER}}
+: ${MOODLE_DB_PASSWORD:=$DB_ENV_POSTGRES_PASSWORD}
+
+if [ -z "$MOODLE_DB_PASSWORD" ]; then
+  echo >&2 'error: missing required MOODLE_DB_PASSWORD environment variable'
+  echo >&2 '  Did you forget to -e MOODLE_DB_PASSWORD=... ?'
+  echo >&2
+  exit 1
+fi
 
 else
   echo >&2 "This database type is not supported"
-  echo >&2 "Did you forget to -e MOODLE_DB_TYPE='mysqli' ^OR^ -e MOODLE_DB_TYPE='mariadb' ?"
+  echo >&2 "Did you forget to -e MOODLE_DB_TYPE='mysqli' ^OR^ -e MOODLE_DB_TYPE='mariadb' ^OR^ -e MOODLE_DB_TYPE='pgsql' ?"
   exit 1
 fi
 
 
+# if [ -z "$MOODLE_DB_PASSWORD" ]; then
+#   echo >&2 'error: missing required MOODLE_DB_PASSWORD environment variable'
+#   echo >&2 '  Did you forget to -e MOODLE_DB_PASSWORD=... ?'
+#   echo >&2
+#   exit 1
+# fi
 
 echo "Installing moodle"
 php /var/www/html/admin/cli/install_database.php \
